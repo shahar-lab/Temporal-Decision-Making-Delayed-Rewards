d72bb37d6e68935e090b71eaa3d749dc22a7556f
NoaShen <NOA.SHEN15@GMAIL.COM>
2023-01-02 18:46:25 +0200

    K function

--- simulate_choice.R
+++ simulate_choice.R
@@ -9,11 +9,15 @@ df <- read.csv('./data/df_raw.csv')
 
 
 simulate_choice <- function (immediate_reward, delayed_reward, individual_k, delay) {
-  sim_choice <- immediate_reward
-  
-}
+  if (delayed_reward/(1+individual_k*delay) > immediate_reward) {
+    sim_choice <- 1
+  } else {
+    sim_choice <- 0
+  }
+  }
 
 
 for(i in 1:nrow(df)) {  
   df$simulate_choice[i] <- simulate_choice(df$offerI[i],df$offerD[i],df$individual_k[i],df$Delay[i])
+  df$v[i] <- simulate_choice(df$offerI[i],df$offerD[i],df$individual_k[i],df$Delay[i])
 }
